7d71863d34315dc349d1681622fec7c25f75ecba
marinarose92 <maffia.marina@gmail.com>
2019-08-12 12:57:48 -0400

    organized code into functions

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-let containerSelector = document.getElementById('container');
-x = 0;
+function renderCanvas() {
+    let containerSelector = document.getElementById('container');
 
-while (x <= 255) {
-    let div = document.createElement('div');
-    containerSelector.appendChild(div);
-    div.id = x;
-    div.classList.add("grid");
-    x++
+    x = 0;
+
+    while (x <= 255) {
+        let div = document.createElement('div');
+        containerSelector.appendChild(div);
+        div.id = x;
+        div.classList.add("grid");
+        x++
+    }
+
+    const gridSelector = document.querySelectorAll('.grid');
+
+    drawingLogic(gridSelector);
 }
+
+function drawingLogic(gridSelector) {
+    gridSelector.forEach(div => {
+        div.addEventListener('mouseover', event => {
+            console.log(event.target.classList.add('blue'));
+        })
+    });
+}
+
+renderCanvas();
