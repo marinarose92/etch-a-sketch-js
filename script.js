494b27906a2d49b9025480096a175469f8d84ccd
marinarose92 <maffia.marina@gmail.com>
2019-08-13 09:41:14 -0400

    finished clearButton logic

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
 function UI() {
     let containerSelector = document.getElementById('container');
     let clearButton = document.getElementById('clear');
-
     clearButton.addEventListener('click', event => {
         let userInput = window.prompt("What size grid do you want?");
-        renderCustomCanvas(userInput);
+        userInput = Number(userInput);
+        renderCanvas(containerSelector, userInput);
     })
-
-    renderInitialCanvas(containerSelector);
+    renderCanvas(containerSelector);
 }
 
-function renderInitialCanvas (containerSelector) {
-    let y = 256;
-    x = 0;
-    while (x <= y) {
-        let div = document.createElement('div');
-        containerSelector.appendChild(div);
-        div.id = x;
-        div.classList.add("grid");
-        x++
-    }
-
-    const gridSelector = document.querySelectorAll('.grid');
-
-    drawingLogic(gridSelector)
-}
-
-function renderCustomCanvas (userInput) {
-    console.log(userInput);
+function renderCanvas (containerSelector, userInput) {
+    let defaultSize = 16;
+        if (userInput === undefined) {
+            defaultSize = 16;
+        } else {
+            defaultSize = userInput;
+            if (defaultSize > 100) {
+                defaultSize = 100;
+            }
+            console.log(defaultSize);
+            containerSelector.innerHTML = "";
+        }
+        x = 0;
+        let square = Math.pow(defaultSize, 2)
+        while (x <= square) {
+            let div = document.createElement('div');
+            containerSelector.appendChild(div);
+            div.id = x;
+            div.style.width = (800 / defaultSize) + 'px';
+            div.style.height = (800 / defaultSize) + 'px';
+            div.style.display = "inline-block";
+            div.classList.add("grid");
+            x++
+        }
+        const gridSelector = document.querySelectorAll('.grid');
+        drawingLogic(gridSelector)
 }
 
 function drawingLogic(gridSelector) {
     gridSelector.forEach(div => {
         div.addEventListener('mouseover', event => {
-            console.log(event.target.classList.add('blue'));
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            div.style.backgroundColor = '#' + randomColor;
         })
     });
 }
